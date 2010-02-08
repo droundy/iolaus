0d4d9bb09f94e88935c197b87267ad35e5103568
David Roundy <roundyd@physics.oregonstate.edu>
2010-02-07 20:03:00 -0800

    a very hokey version of whatsnew.
    Tested-on: morland i686 GNU/Linux

--- src/git/plumbing.go
+++ src/git/plumbing.go
@@ -4,10 +4,58 @@ import (
 	"./git"
 	"os"
 	"strings"
+	"patch"
+	"fmt"
 )
 
 func Init() {
 	git.Run("init",nil)
+}
+
+type Patch patch.Set
+
+func Diff(paths []string) Patch {
+	o, _ := git.Read("diff", []string{})
+	p, _ := patch.Parse(strings.Bytes(o));
+	return Patch(*p)
+}
+
+func (s Patch) String() (out string) {
+	out = s.Header
+	for _, f := range s.File {
+		if len(out) > 0 { out += "\n" }
+		out += string(f.Verb) + " " + f.Src
+		if f.OldMode != 0 || f.NewMode != 0 {
+			out += "\n" + fmt.Sprint(f.OldMode) + " -> " + fmt.Sprint(f.NewMode)
+		}
+		switch d := f.Diff.(type) {
+		default:
+			out += fmt.Sprintf("\nunexpected type %T", f.Diff)  // %T prints type
+		case patch.TextDiff:
+			for _, chunk := range d {
+				out += "\n" + fmt.Sprintln(chunk.Line)
+				older := strings.SplitAfter(string(chunk.Old), "\n",0)
+				newer := strings.SplitAfter(string(chunk.New), "\n",0)
+				for i, l := range older {
+					if len(l)>0 {
+						if i>=len(newer) || newer[i] != l {
+							out += "-" + l
+						} else {
+							out += " " + l
+						}
+					}
+				}
+				for i, l := range newer {
+					if len(l)>0 {
+						if i>=len(older) || older[i] != l {
+							out += "+" + l
+						}
+					}
+				}
+			}
+		}
+	}
+	return
 }
 
 func LsFiles() (fs []string, e os.Error) {
--- src/iolaus-whatsnew.go
+++ src/iolaus-whatsnew.go
@@ -1,14 +1,17 @@
 package main;
 
-import "./git/git"
-import "./git/plumbing"
-import "./util/error"
-import "./util/help"
+import (
+	"./git/git"
+	"./git/plumbing"
+	"./util/error"
+	"./util/help"
+)
 
 func main() {
 	git.AmInRepo("Must be in a repository to call whatsnew!")
 	help.Init("see unrecorded changes.")
 	fs,_ := plumbing.LsFiles()
 	error.Print(fs)
-	error.Print("This isn't working yet, obviously...")
+	p := plumbing.Diff([]string{})
+	error.Print(p)
 }
