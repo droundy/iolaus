40be8f67a8f1c80f5459f32dad6a8da7c265351a
David Roundy <roundyd@physics.oregonstate.edu>
2010-03-04 09:58:18 -0800

    avoid excess newline in whatsnew when there are no changes.
    Tested-on: morland i686 GNU/Linux

--- src/iolaus-whatsnew.go
+++ src/iolaus-whatsnew.go
@@ -11,7 +11,10 @@ func main() {
 	git.AmInRepo("Must be in a repository to call whatsnew!")
 	help.Init("see unrecorded changes.", plumbing.LsFiles)
 	//plumbing.ReadTree(plumbing.Ref("HEAD"))
-	out.Print(plumbing.DiffFiles([]string{}))
+	p := plumbing.DiffFiles([]string{}).String()
+	if p != "" {
+		out.Print(p)
+	}
 	for _,newf := range plumbing.LsOthers() {
 		out.Print("Added ",newf)
 	}
