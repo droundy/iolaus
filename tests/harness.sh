0c6932f6c16656dca5dd226b72670d3a378265da
David Roundy <roundyd@physics.oregonstate.edu>
2010-07-02 17:20:35 -0700

    make harness.sh test a bit trickier.
    Tested-on: bingley (Linux 2.6.32-5-amd64)

--- tests/harness.sh
+++ tests/harness.sh
@@ -4,6 +4,13 @@ set -ev
 
 # verify that the harness is working properly
 
+HERE=`pwd`
+cd ../../..
+GRANDPARENT=`pwd`
+cd "$HERE"
+# The current directory
+grep "$GRANDPARENT" $(which iolaus-initialize)
+
 cd $(dirname $(which iolaus-initialize))
 pwd
 test -d ../src
